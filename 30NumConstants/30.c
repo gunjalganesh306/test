5d1870146e850bf009c9816a39f5e92873d6f436
gunjalganesh306 <gunjalganesh306@gmail.com>
2017-11-22 15:51:56 +0530

    testing

--- 30NumConstants/30.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#define e 0
-void main()
-{
-	int a = 0, b = 0, c = 0, d = 0;
-	scanf("%d%d",&a,&b);
-	c = a + b;
-	printf(c);
-}
--- 34LexPrgForCPrg/34.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-	int a = 0, b = 0, c = 0;
-	scanf("%d%d",&a,&b);
-	c = a + b;
-	printf(c);
-}
